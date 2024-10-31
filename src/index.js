34bb875012325c218eb1017a7c7f3c343462a15f
northernomad <kanyshbeisenbek@gmail.com>
2024-10-31 23:07:41 +0500

    step 5 JSX

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react"
 import ReactDOM from 'react-dom'
 
-//const element = React.createElement('hi', null, 'Hello React World');
+
+
 
 const TodoList = () => {
+  const items = ['Install React', 'Study Reac', 'Use React', 'Build React App'];
   return (
     <ul>
-      <li>Install React</li>
-      <li>Study React</li>
-      <li>Use React</li>
-      <li>Build React App</li>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+      <li>{items[2]}</li>
+      <li>{items[3]}</li>
     </ul>
     )
 }
@@ -18,9 +20,31 @@ const AppHeader = () => {
   return <h1>My Todo List</h1>
 }
 
+const SearchPanel = () => {
+  
+  const searchText = 'Type here to search'
+  const searctStyle = {
+    fontSize: '20px'
+  }
+
+  // return <input placeholder="search" />;
+  return <input
+    style = {searctStyle}
+    placeholder = {searchText}
+    disabled = {true} />
+}
+
 const App = () => {
+
+  const value = '<script>alert("")</script>'
+  // const isLoggedIn = false;
+  // const loginBox = <span>Log in to the system</span>;
+  // const welcomeBox = <span>Welcome Back</span>;
+
   return (
     <div>
+      {/*{isLoggedIn ? null : loginBox}*/}
+      { value }
       <AppHeader />
       <SearchPanel />
       <TodoList/>
