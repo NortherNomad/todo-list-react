7174969491d555e9814e1568fc61962d0ef06181
northernomad <kanyshbeisenbek@gmail.com>
2024-10-31 23:00:33 +0500

    step 4 React Components creating

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import React from "react"
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 
 //const element = React.createElement('hi', null, 'Hello React World');
 
-const element = (
-  <div>
-    <h1>My Todo List</h1>
-    <input placeholder="search" />
+const TodoList = () => {
+  return (
     <ul>
       <li>Install React</li>
       <li>Study React</li>
       <li>Use React</li>
       <li>Build React App</li>
     </ul>
-  </div>
-)
+    )
+}
 
-ReactDom.render(
-  element,
-  document.getElementById('root')
-)
+const AppHeader = () => {
+  return <h1>My Todo List</h1>
+}
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root'))
