0fa38ef994d07d7366a24647415c67f5a27d60d4
northernomad <kanyshbeisenbek@gmail.com>
2024-11-01 12:41:59 +0500

    step 21 Changes state component and use true for done

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,18 +3,28 @@ import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
 
+    state = {
+        done: true
+    }
+
     onLabelClick = () => {
         console.log(`Done: ${this.props.label}`)
     }
 
     render () {
         const {label, important=false} = this.props
+        const {done} = this.state
+
+        let classNames = 'todo-list-item'
+        if (done) {
+            classNames += ' done'
+        }
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         }
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
                 <span
                     className="todo-list-item-label"
                     style={style}
